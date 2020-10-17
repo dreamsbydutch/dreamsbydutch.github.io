ba7c6a9184ec4c763e3591803809da6f7f393584
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-16 20:33:33 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
-$.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/od6/public/values?alt=json", function(data) {
-  //first row "title" column
-  $('#second-team-name').load(data.feed.entry[0]['gsx$title']['$t']);
-});
-  
-  
+// Define spreadsheet URL.
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
+
+// Load an entire worksheet.
+$('#second-team-name').sheetrock({
+  url: mySpreadsheet
