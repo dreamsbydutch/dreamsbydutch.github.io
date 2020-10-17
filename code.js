6e7ca6dd11b4b8b3934ee0bf596e0f8eb70b0674
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-16 21:44:33 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,8 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
 
 // Load an entire worksheet.
-$('#second-team-name').sheetrock({
+sheetrock({
   url: mySpreadsheet,
-  query: "select A where B = '2'"
+  query: "select A where B = '2'",
+  target: document.getElementById('thirdTeamName')
 });
