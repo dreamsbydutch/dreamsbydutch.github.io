b3585f21a69e510283c327156aa1eb6a2cda06a0
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-18 14:19:37 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
 var rankingTemplate = Handlebars.compile($('#ranking-template').html());
+var i;
 
 $('#rankings-table').sheetrock({
       url: mySpreadsheet,
