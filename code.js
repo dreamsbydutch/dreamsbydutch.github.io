1f8e9864c966cce16a5793176b337d00479cf59a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-18 14:55:59 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,9 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
 var rankingTemplate = Handlebars.compile($('#ranking-template').html());
-var i;
 
 $('#rankings-table').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,G,H,O,Q order by G desc",
+      query: "select A,B,C,D,E,F,H,I,P,R order by A asc",
       rowTemplate: rankingTemplate
 });
