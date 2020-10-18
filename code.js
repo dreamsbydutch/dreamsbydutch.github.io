4c35fac6692b0d92bf226a67f662302107de4b44
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-18 11:24:27 -0400

    Update code.js

--- code.js
+++ code.js
@@ -4,12 +4,68 @@ var i;
 var textID;
 
 for (i = 1; i < 17; i++) {
-  // Load an entire worksheet.
-  $('#teamName'+i).sheetrock({
+  
+  // Enter Rank Gains
+  $('#rankRankGain'+i).sheetrock({
     url: mySpreadsheet,
-    query: "select A where B = " + i,
+    query: "select B where A = " + i,
     callback: function (error, options, response) {
       console.log(error, options, response);
     }
   });
+    
+  // Enter Team Logos
+  $('#rankTeamLogo'+i).attr("src", sheetrock({
+    url: mySpreadsheet,
+    query: "select C where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  }));
+    
+  // Enter Team Records
+  $('#rankTeamRecord'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select D where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+    
+  // Enter Team Scores
+  $('#rankTeamScore'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select E where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+      
+  // Enter Team Best Players
+  $('#rankTeamBestPlayer'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select F where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+      
+  // Enter Team Last Weeks
+  $('#rankTeamLastWeek'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select G where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+      
+  // Enter Team Streaks
+  $('#rankTeamStreak'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select H where A = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+  
 }
