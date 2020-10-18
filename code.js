952e91ab68e49ff61bddd98fd8642032734e8b6c
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-18 10:59:01 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,15 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
+var i;
+var textID;
 
-// Load an entire worksheet.
-$('#thirdTeamName').sheetrock({
-  url: mySpreadsheet,
-  query: "select A where B = 2",
-  callback: function (error, options, response) {
-    console.log(error, options, response);
-  }
-});
+for (i = 1; i < 17; i++) {
+  // Load an entire worksheet.
+  $('#teamName'+i).sheetrock({
+    url: mySpreadsheet,
+    query: "select A where B = " + i,
+    callback: function (error, options, response) {
+      console.log(error, options, response);
+    }
+  });
+}
