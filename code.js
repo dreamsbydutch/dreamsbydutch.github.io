b83c1a7f363451cdb0ed9daf14d5ce6129d9fd4a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-18 12:20:23 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,80 +2,58 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=0';
 var i;
 
+myCallback = function (error, options, response) {
+      console.log(error, options, response);
+    };
+
 for (i = 1; i < 17; i++) {
   
   // Enter Rank Gains
   $('#rankTeamRankGain'+i).sheetrock({
     url: mySpreadsheet,
     query: "select B where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
-    
-  // Enter Team Logos
-  var logoLink = sheetrock({
-    url: mySpreadsheet,
-    query: "select C where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
-  });
-  var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
-  img.attr('src', logoLink);
-  img.appendTo('#rankTeamLogo'+i);
     
   // Enter Team Names
   $('#rankTeamName'+i).sheetrock({
     url: mySpreadsheet,
     query: "select D where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
   
   // Enter Team Records
   $('#rankTeamRecord'+i).sheetrock({
     url: mySpreadsheet,
     query: "select E where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
     
   // Enter Team Scores
   $('#rankTeamScore'+i).sheetrock({
     url: mySpreadsheet,
     query: "select G where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
       
   // Enter Team Best Players
   $('#rankTeamBestPlayer'+i).sheetrock({
     url: mySpreadsheet,
     query: "select H where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
       
   // Enter Team Last Weeks
   $('#rankTeamLastWeek'+i).sheetrock({
     url: mySpreadsheet,
     query: "select O where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   });
       
   // Enter Team Streaks
   $('#rankTeamStreak'+i).sheetrock({
     url: mySpreadsheet,
     query: "select Q where A = " + i,
-    callback: function (error, options, response) {
-      console.log(error, options, response);
-    }
+    callback: myCallback
   }); 
 }
