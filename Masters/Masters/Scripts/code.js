6f47b7c18032e43f7ba9c1ce8ecab5c5a6b181f2
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-22 13:58:44 -0400

    Delete code.js

--- Masters/Masters/Scripts/code.js
+++
@@ -1,9 +0,0 @@
-// Define spreadsheet URL.
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1NJZbWIvFYCTIt98BdxOZK1Z43XEOvqU6zqST_KJeJh0/edit#gid=0';
-var leaderboardTemplate = Handlebars.compile($('#leaderboard-template').html());
-
-$('#leaderboard').sheetrock({
-      url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N order by D desc",
-      rowTemplate: leaderboardTemplate
-});
