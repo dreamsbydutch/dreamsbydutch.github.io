5bb9762effeb237843f8381493f5192ea8b74973
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-22 16:15:51 -0400

    Update code.js

--- Masters/Scripts/code.js
+++ Masters/Scripts/code.js
@@ -4,6 +4,6 @@ var leaderboardTemplate = Handlebars.compile($('#leaderboard-template').html());
 
 $('#leaderboard').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q order by Q desc",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q order by Q asc",
       rowTemplate: leaderboardTemplate
 });
