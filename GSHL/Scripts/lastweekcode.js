044ad195f51fb5ec76c1b616140ec68fb84b796a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-27 16:25:21 -0400

    Update lastweekcode.js

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -1,22 +1,23 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=34196491';
 var matchupTemplate = Handlebars.compile($('#matchup-template').html());
+var i = 4;
 
 $('#top-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B = 1",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = " + i + " and B = 1",
       rowTemplate: matchupTemplate
 });
 
 $('#other-matchups').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B != 1 and B != 8 order by B asc",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = " + i + " and B != 1 and B != 8 order by B asc",
       rowTemplate: matchupTemplate
 });
 
 $('#bottom-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B  = 8",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = " + i + " and B  = 8",
       rowTemplate: matchupTemplate
 });
 
