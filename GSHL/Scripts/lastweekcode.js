a07f284b82ff70469ffe3938e95dcdfca6e99cf9
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-27 15:52:05 -0400

    Update lastweekcode.js

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -4,18 +4,30 @@ var matchupTemplate = Handlebars.compile($('#matchup-template').html());
 
 $('#top-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP where A = 4 and B = 1",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B = 1",
       rowTemplate: matchupTemplate
 });
 
 $('#other-matchups').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP where A = 4 and B != 1 and B != 8 order by B asc",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B != 1 and B != 8 order by B asc",
       rowTemplate: matchupTemplate
 });
 
 $('#bottom-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP where A = 4 and B  = 8",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,CQ,CR,CS,CT,CU,CV,CW,CX,CY,CZ,DA where A = 4 and B  = 8",
       rowTemplate: matchupTemplate
+});
+
+Handlebars.registerHelper('catWinlossColor', function catWinlossColor(winLoss) {
+      s = winLoss.toString();
+      s = s.charAt(0);
+      if (s == "W") {
+            return '#006700';
+      } else if (s == "L") {
+            return '#9a0000';
+      } else {
+            return '#222';
+      }
 });
