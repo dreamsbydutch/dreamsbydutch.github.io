ffc607146afff03024004e42d7c0fa2556a7b82a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-27 10:36:05 -0400

    Update lastweekcode.js

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -4,6 +4,6 @@ var matchupTemplate = Handlebars.compile($('#matchup-template').html());
 
 $('.matchup-box').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP where A = 4 and B = 1",
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP where A = 4 order by B asc",
       rowTemplate: matchupTemplate
 });
