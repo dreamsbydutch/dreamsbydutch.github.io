240623fc6c49a046ced4c2898eba8d10a901dff9
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-26 21:43:53 -0400

    Update lastweekcode.js

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -2,7 +2,7 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=34196491';
 var matchupTemplate = Handlebars.compile($('#matchup-template').html());
 
-$('#matchupbox').sheetrock({
+$('.matchup-box').sheetrock({
       url: mySpreadsheet,
       query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,AY,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN where A = '4' order by B asc",
       rowTemplate: matchupTemplate
