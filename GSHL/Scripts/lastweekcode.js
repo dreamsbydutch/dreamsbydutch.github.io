8d63ecfc64979546640af8ef4bdd53aaa5c7554d
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-28 13:45:40 -0400

    Update lastweekcode.js

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -1,6 +1,7 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=34196491';
 var matchupTemplate = Handlebars.compile($('#matchup-template').html());
+var nextMatchupTemplate = Handlebars.compile($('#next-matchup-template').html());
 var i = 4;
 
 $('#top-matchup').sheetrock({
@@ -20,8 +21,6 @@ $('#bottom-matchup').sheetrock({
       query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AT,AU,AV,AW,AX,AY,AZ,BA,BB,BC,BD,BE,BF,BG,BH,BI,BJ,CE,CF,CG,CH,CI,CJ,CK,CL,CM,CN,CO where A = " + i + " and B  = 8",
       rowTemplate: matchupTemplate
 });
-
-var nextMatchupTemplate = Handlebars.compile($('#next-matchup-template').html());
 
 $('#top-next-matchup').sheetrock({
       url: mySpreadsheet,
