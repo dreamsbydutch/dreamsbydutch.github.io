f1e032f115e43e8cf7a656636e8baec6988a691e
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-28 15:40:32 -0400

    Update thisweekcode.js

--- GSHL/Scripts/thisweekcode.js
+++ GSHL/Scripts/thisweekcode.js
@@ -5,6 +5,6 @@ var i = 5;
 
 $('#top-next-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA where A = " + i + " and B = 1",
+      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV where A = " + i + " and B = 1",
       rowTemplate: matchupTemplateNext
 });
