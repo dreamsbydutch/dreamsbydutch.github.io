942f0b4fc502717db3cb6e3ac50ad69b9a5d2508
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-28 15:18:51 -0400

    Update thisweekcode.js

--- GSHL/Scripts/thisweekcode.js
+++ GSHL/Scripts/thisweekcode.js
@@ -1,23 +1,22 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=34196491';
-var nextMatchupTemplate = Handlebars.compile($('#next-match-template').html());
+var matchupTemplateNext = Handlebars.compile($('#next-match-template').html());
 var i = 5;
 
 $('#top-next-matchup').sheetrock({
       url: mySpreadsheet,
       query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B = 1",
-      rowTemplate: nextMatchupTemplate
+      rowTemplate: matchupTemplateNext
 });
 
 $('#other-next-matchups').sheetrock({
       url: mySpreadsheet,
       query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B != 1 and B != 8 order by B asc",
-      rowTemplate: nextMatchupTemplate
+      rowTemplate: matchupTemplateNext
 });
 
 $('#bottom-next-matchup').sheetrock({
       url: mySpreadsheet,
-      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B  = 8",
-      rowTemplate: nextMatchupTemplate
+      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B = 8",
+      rowTemplate: matchupTemplateNext
 });
-
