0b7c1f2dd960a60c4c73dc451cac47529f124fa8
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-10-28 15:19:58 -0400

    Update thisweekcode.js

--- GSHL/Scripts/thisweekcode.js
+++ GSHL/Scripts/thisweekcode.js
@@ -8,15 +8,3 @@ $('#top-next-matchup').sheetrock({
       query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B = 1",
       rowTemplate: matchupTemplateNext
 });
-
-$('#other-next-matchups').sheetrock({
-      url: mySpreadsheet,
-      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B != 1 and B != 8 order by B asc",
-      rowTemplate: matchupTemplateNext
-});
-
-$('#bottom-next-matchup').sheetrock({
-      url: mySpreadsheet,
-      query: "select A,B,I,J,K,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW,BX,BY,BZ,CA,CB,CC,CD where A = " + i + " and B = 8",
-      rowTemplate: matchupTemplateNext
-});
