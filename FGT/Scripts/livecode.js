514a2565751d8e83566c2bb6d2c63612f88fe580
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-11-27 13:13:56 -0500

    f

---
+++ FGT/Scripts/livecode.js
@@ -0,0 +1,16 @@
+var mySpreadsheetHeader = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1192860014';
+var mySpreadsheetLeaderboard = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=304596389';
+var headerTemplate = Handlebars.compile($('#tournament-header-template').html());
+var leaderboardTemplate = Handlebars.compile($('#tournament-leaderboard-template').html());
+
+$('#tournament-header').sheetrock({
+  url: mySpreadsheetHeader,
+  query: "select A,B,C,D,E,F",
+  rowTemplate: headerTemplate
+});
+
+$('#tournament-leaderboard').sheetrock({
+      url: mySpreadsheetLeaderboard,
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V order by A asc",
+      rowTemplate: leaderboardTemplate
+});
