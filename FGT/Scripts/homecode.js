e45ba4260d384a153346b3cc51756c0bc3717ec1
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-11-27 05:53:11 -0500

    l

--- FGT/Scripts/homecode.js
+++ FGT/Scripts/homecode.js
@@ -1,5 +1,6 @@
 var images = ['Images/wallpaper/wallpaper1.jpg', 'Images/wallpaper/wallpaper2.jpg', 'Images/wallpaper/wallpaper3.jpg', 'Images/wallpaper/wallpaper4.jpg', 'Images/wallpaper/wallpaper5.jpg', 'Images/wallpaper/wallpaper6.jpg', 'Images/wallpaper/wallpaper7.jpg'];
-$('html').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + ')'});
+$('html').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + ');'});
+$('html').css({'background-repeat': 'no-repeat, no-repeat, no-repeat, repeat;'});
 
 var slideIndex = 1;
 showDivs(slideIndex);
