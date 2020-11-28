b87b3ca0b279c8c096e5ce3f90cb0d723f370cee
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-11-27 21:07:24 -0500

    s

---
+++ FGT/Scripts/standingscode.js
@@ -0,0 +1,11 @@
+var images = ['Images/wallpaper/wallpaper1.jpg', 'Images/wallpaper/wallpaper2.jpg', 'Images/wallpaper/wallpaper3.jpg', 'Images/wallpaper/wallpaper4.jpg', 'Images/wallpaper/wallpaper5.jpg', 'Images/wallpaper/wallpaper6.jpg', 'Images/wallpaper/wallpaper7.jpg'];
+$('html').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+var mySpreadsheetStandings = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=0';
+var standingsTemplate = Handlebars.compile($('#standings-template').html());
+
+$('#standings-list').sheetrock({
+  url: mySpreadsheetStandings,
+  query: "select A,B,C,D,E,F,G,H order by A asc",
+  rowTemplate: standingsTemplate
+});
