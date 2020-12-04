30c236543b0ea25c372c0b90b6c1c069d5f2bdf5
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-04 18:35:54 -0500

    Update draftrankingscode.js

--- GSHL/Scripts/draftrankingscode.js
+++ GSHL/Scripts/draftrankingscode.js
@@ -1,5 +1,5 @@
 var mySpreadsheetDraftboardrankings = 'https://docs.google.com/spreadsheets/d/1dxStC3BMhy1ZpiG8Tr5wIkPpZ-Tm0YNYeqjd2PbG5D0/edit#gid=1005494927';
-var draftboardRankingsTemplate = Handlebars.compile($('#draftboard-overall-template').html());
+var draftboardRankingsTemplate = Handlebars.compile($('#draftboard-rankings-overall-template').html());
 
 $('#draftboard-rankings-overall').sheetrock({
     url: mySpreadsheetDraftboardrankings,
