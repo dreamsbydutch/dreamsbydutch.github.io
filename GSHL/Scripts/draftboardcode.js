afb748b592a99f000105a7ecb72d8caa1b7f21e3
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-04 15:39:01 -0500

    d

--- GSHL/Scripts/draftboardcode.js
+++ GSHL/Scripts/draftboardcode.js
@@ -2,7 +2,14 @@ var images = ['Images/wallpaper/wallpaper1.jpg', 'Images/wallpaper/wallpaper2.jp
 $('html').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + ')'});
 
 var mySpreadsheetDraftboard = 'https://docs.google.com/spreadsheets/d/1dxStC3BMhy1ZpiG8Tr5wIkPpZ-Tm0YNYeqjd2PbG5D0/edit#gid=306525779';
+var draftboardHeaderTemplate = Handlebars.compile($('#draftboard-header-template').html());
 var draftboardTemplate = Handlebars.compile($('#draftboard-template').html());
+
+$('#draftboard-header').sheetrock({
+      url: mySpreadsheetDraftboard,
+      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q order by A asc",
+      rowTemplate: draftboardHeaderTemplate
+});
 
 $('#draftboard').sheetrock({
       url: mySpreadsheetDraftboard,
