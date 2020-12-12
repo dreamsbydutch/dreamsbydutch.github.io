ce90ff3b4d0075ffb70e1ed693caa8a78cf10e09
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-12 17:17:34 -0500

    g

--- GSHL/Scripts/rankcode.js
+++ GSHL/Scripts/rankcode.js
@@ -30,3 +30,20 @@ Handlebars.registerHelper('dynamicStreakColor', function dynamicStreakColor(winL
             return '#222';
       }
 });
+
+function showTeam(rank) {
+      var x = document.getElementById("slot-"+rank+"-extras");
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else {
+        x.className = x.className.replace(" w3-show", "");
+      }
+    }
+
+function openSidebar() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeSidebar() {
+  document.getElementById("mySidebar").style.display = "none";
+}
--- GSHL/Scripts/rankfunctions.js
+++
@@ -1,8 +0,0 @@
-function showTeam(rank) {
-    var x = document.getElementById("slot-"+rank+"-extras");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
