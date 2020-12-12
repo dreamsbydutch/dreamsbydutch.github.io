e884a45e689e44c0995d4365aa9d2b29a80b787a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-12 15:07:36 -0500

    Update rankfunctions.js

--- GSHL/Scripts/rankfunctions.js
+++ GSHL/Scripts/rankfunctions.js
@@ -1,5 +1,5 @@
 function showTeam(rank) {
-    var x = document.getElementById("slot-"+id+"-extras");
+    var x = document.getElementById("slot-"+rank+"-extras");
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
     } else {
