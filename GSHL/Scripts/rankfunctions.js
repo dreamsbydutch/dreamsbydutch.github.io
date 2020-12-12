92bdb355bf8c6a7c8ce1239dbae62bc21cb36de5
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-12 13:28:49 -0500

    s

--- GSHL/Scripts/rankfunctions.js
+++ GSHL/Scripts/rankfunctions.js
@@ -1,6 +1,4 @@
 function showTeam(rank) {
     document.getElementById("slot-"+rank).style.gridtemplaterows = "3%, 2%";
-    document.getElementById("slot-"+rank).getElementsByClassName("team-bestplayer").style.visibility ="show";
-    document.getElementById("slot-"+rank).getElementsByClassName("team-lastweek").style.visibility ="show";
-    document.getElementById("slot-"+rank).getElementsByClassName("team-streak").style.visibility ="show";
+    document.getElementsByClassName("show-slot-"+rank).style.visibility = "show";
 }
