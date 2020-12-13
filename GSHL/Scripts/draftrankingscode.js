42c62ea44517e1213e29bc9b9edcbd3a7ad7bbb2
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-13 14:21:48 -0500

    Update draftrankingscode.js

--- GSHL/Scripts/draftrankingscode.js
+++ GSHL/Scripts/draftrankingscode.js
@@ -1,19 +1,3 @@
-var mySpreadsheetDraftboard = 'https://docs.google.com/spreadsheets/d/1dxStC3BMhy1ZpiG8Tr5wIkPpZ-Tm0YNYeqjd2PbG5D0/edit#gid=306525779';
-var draftboardHeaderTemplate = Handlebars.compile($('#draftboard-header-template').html());
-var draftboardTemplate = Handlebars.compile($('#draftboard-template').html());
-
-$('#draftboard-header').sheetrock({
-      url: mySpreadsheetDraftboard,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q where A = 0 order by A asc",
-      rowTemplate: draftboardHeaderTemplate
-});
-
-$('#draftboard-listing').sheetrock({
-      url: mySpreadsheetDraftboard,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q where A > 0 order by A asc",
-      rowTemplate: draftboardTemplate
-});
-
 var mySpreadsheetDraftdayrankings = 'https://docs.google.com/spreadsheets/d/1dxStC3BMhy1ZpiG8Tr5wIkPpZ-Tm0YNYeqjd2PbG5D0/edit#gid=1005494927';
 var draftdayRankingsTemplate = Handlebars.compile($('#draftday-rankings-overall-template').html());
 
