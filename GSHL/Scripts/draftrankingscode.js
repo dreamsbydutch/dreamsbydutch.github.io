ae153f9fc5c9ef1204689e8399e384d68724d9f7
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-13 21:25:17 -0500

    s

--- GSHL/Scripts/draftrankingscode.js
+++ GSHL/Scripts/draftrankingscode.js
@@ -1,39 +1,39 @@
-var mySpreadsheetDraftdayrankings = 'https://docs.google.com/spreadsheets/d/1dxStC3BMhy1ZpiG8Tr5wIkPpZ-Tm0YNYeqjd2PbG5D0/edit#gid=1005494927';
+var mySpreadsheetDraftdayrankings = 'https://docs.google.com/spreadsheets/d/1RDPMwBI77W_z9CvI2nVG6kTc6Q9J6vYrcE5boD-RiVE/edit#gid=331802598';
 var draftdayRankingsTemplate = Handlebars.compile($('#draftday-rankings-overall-template').html());
 
 $('#draftday-rankings-overall').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I order by A asc",
+    query: "select A,B,C,D,V,W order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
 $('#draftday-rankings-center').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I where H = 'C' order by A asc",
+    query: "select A,B,C,D,V,W where contains(D,'C') order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
 $('#draftday-rankings-leftwing').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I where H = 'LW' order by A asc",
+    query: "select A,B,C,D,V,W where D = 'LW' order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
 $('#draftday-rankings-rightwing').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I where H = 'RW' order by A asc",
+    query: "select A,B,C,D,V,W where D = 'RW' order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
 $('#draftday-rankings-defense').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I where H = 'D' order by A asc",
+    query: "select A,B,C,D,V,W where D = 'D' order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
 $('#draftday-rankings-goalies').sheetrock({
     url: mySpreadsheetDraftdayrankings,
-    query: "select A,B,C,D,E,F,G,H,I where H = 'G' order by A asc",
+    query: "select A,B,C,D,V,W where D = 'G' order by A asc",
     rowTemplate: draftdayRankingsTemplate
 });
 
