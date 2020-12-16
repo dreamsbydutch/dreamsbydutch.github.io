0a32736664e6c9a15c7a69bb599cbf8e623ca744
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2020-12-15 21:35:04 -0500

    t

--- GSHL/Scripts/rulescode.js
+++
@@ -1,8 +0,0 @@
-
-function openSidebar() {
-    document.getElementById("mySidebar").style.display = "block";
-  }
-  
-  function closeSidebar() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
