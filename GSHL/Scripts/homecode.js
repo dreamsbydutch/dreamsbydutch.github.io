23c38cad730fc65a7f3249765cff11accc9b755b
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-03 17:09:08 -0500

    D

--- GSHL/Scripts/homecode.js
+++ GSHL/Scripts/homecode.js
@@ -1,3 +1,13 @@
+// Define spreadsheet URL.
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1GNYs0bH1wOSqrZEAlH9LtCTlcZ6ruMPrrq15umFAiMQ/edit#gid=1368254096';
+var standingTemplate = Handlebars.compile($('#matchups-template').html());
+
+$('#home-matchups').sheetrock({
+      url: mySpreadsheet,
+      query: "select B,C,J,L,M,AG,AI,AJ where C = 'LW' order by B asc",
+      rowTemplate: standingTemplate
+});
+
 function openSidebar() {
     document.getElementById("mySidebar").style.display = "block";
   }
