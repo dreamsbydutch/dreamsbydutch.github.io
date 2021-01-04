e4e9ce7d4ee622a1e289bec02441f8beeee1cb42
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-03 21:17:12 -0500

    d

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -1,3 +1,13 @@
+// Define spreadsheet URL.
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1GNYs0bH1wOSqrZEAlH9LtCTlcZ6ruMPrrq15umFAiMQ/edit#gid=1368254096';
+var standingTemplate = Handlebars.compile($('#matchups-template').html());
+
+$('#matchups').sheetrock({
+      url: mySpreadsheet,
+      query: "select B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,A,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,AX,AY,AZ,BA,BB,BC where C = 'LW' order by B asc",
+      rowTemplate: standingTemplate
+});
+
 function openSidebar() {
     document.getElementById("mySidebar").style.display = "block";
   }
