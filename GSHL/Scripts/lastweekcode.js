cff033bcd4e7f039d3cdb571c1625f512cf4f89a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-03 21:30:57 -0500

    d

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -4,7 +4,7 @@ var standingTemplate = Handlebars.compile($('#matchups-template').html());
 
 $('#matchups').sheetrock({
       url: mySpreadsheet,
-      query: "select B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,A,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,AX,AY,AZ,BA,BB,BC where C = 'LW' order by B asc",
+      query: "select B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV where C = 'LW' order by B asc",
       rowTemplate: standingTemplate
 });
 
