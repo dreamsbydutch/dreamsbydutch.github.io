583d92bdd81caaba6069aece1b8c5920171adaa5
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-03 23:33:19 -0500

    f

--- GSHL/Scripts/lastweekcode.js
+++ GSHL/Scripts/lastweekcode.js
@@ -4,8 +4,16 @@ var standingTemplate = Handlebars.compile($('#matchups-template').html());
 
 $('#matchups').sheetrock({
       url: mySpreadsheet,
-      query: "select B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV where C = 'LW' order by B asc",
+      query: "select B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q,R,S,T,U,V,W,X,Y,AG,AH,AI,AJ,AK,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,BD,BE,BF,BG,BH,BI,BJ,BK,BL,BM,BN,BO,BP,BQ,BR,BS,BT,BU,BV,BW where C = 'LW' order by B asc",
       rowTemplate: standingTemplate
+});
+
+Handlebars.registerHelper('catWinner', function catWinner(winvar) {
+  if (winvar) {
+        return 'color:#059d0a;font-weight:bold;';
+  } else {
+        return '#222';
+  }
 });
 
 function openSidebar() {
