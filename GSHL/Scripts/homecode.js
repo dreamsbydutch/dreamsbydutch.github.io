047c4dae62505243fedfbdb80a43daeb5bbd5c77
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-08 18:53:50 -0500

    s

--- GSHL/Scripts/homecode.js
+++ GSHL/Scripts/homecode.js
@@ -1,7 +1,9 @@
 // Define spreadsheet URL.
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1GNYs0bH1wOSqrZEAlH9LtCTlcZ6ruMPrrq15umFAiMQ/edit#gid=1368254096';
+var draftSpreadsheet = '';
 var standingTemplate = Handlebars.compile($('#matchups-template').html());
 var standingNextTemplate = Handlebars.compile($('#next-matchups-template').html());
+var draftTemplate = Handlebars.compile($('#draftorder').html());
 
 $('#home-matchups').sheetrock({
       url: mySpreadsheet,
@@ -14,6 +16,12 @@ $('#home-next-matchups').sheetrock({
   query: "select B,C,J,L,AG,AI where C = 'TW' order by B asc",
   rowTemplate: standingNextTemplate
 });
+
+$('#draftorder').sheetrock({
+  url: draftSpreadsheet,
+  query: "select A,B,C,D order by D asc",
+  rowTemplate: draftTemplate
+})
 
 Handlebars.registerHelper('winChangeBold', function winChangeBold(change) {
   if (change === "W") {
