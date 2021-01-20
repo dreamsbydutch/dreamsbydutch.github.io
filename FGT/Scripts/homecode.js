98f01533a6b8ba53703758e77e1ae6295613f047
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-20 15:36:53 -0500

    w

--- FGT/Scripts/homecode.js
+++ FGT/Scripts/homecode.js
@@ -1,42 +1,9 @@
-var images = ['Images/wallpaper/wallpaper1.jpg', 'Images/wallpaper/wallpaper2.jpg', 'Images/wallpaper/wallpaper3.jpg', 'Images/wallpaper/wallpaper4.jpg', 'Images/wallpaper/wallpaper5.jpg', 'Images/wallpaper/wallpaper6.jpg', 'Images/wallpaper/wallpaper7.jpg'];
-$('html').css({'background-image': 'url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + '), url(' + images[Math.floor(Math.random() * images.length)] + ')'});
-
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
-
 var mySpreadsheetOverall = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=0';
 var mySpreadsheetSchedule = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1192860014';
 var mySpreadsheetWorldRank = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=119653565';
-var mySpreadsheetHeadlines = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1066263828';
 var overallStandingsTemplate = Handlebars.compile($('#cup-standings-template').html());
 var scheduleTemplate = Handlebars.compile($('#schedule-template').html());
 var worldrankTemplate = Handlebars.compile($('#worldrank-template').html());
-var headlinesTemplate = Handlebars.compile($('#headlines-template').html());
 
 $('#standings-slots').sheetrock({
   url: mySpreadsheetOverall,
@@ -56,12 +23,6 @@ $('#worldrank-list').sheetrock({
   rowTemplate: worldrankTemplate
 });
 
-$('#headlines-list').sheetrock({
-  url: mySpreadsheetHeadlines,
-  query: "select A,B,C",
-  rowTemplate: headlinesTemplate
-});
-
 Handlebars.registerHelper('rankColorChange', function rankColorChange(change) {
       var i = parseInt(change);
       if (i > 0) {
@@ -72,3 +33,11 @@ Handlebars.registerHelper('rankColorChange', function rankColorChange(change) {
             return '#222';
       }
 });
+
+function openSidebar() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
