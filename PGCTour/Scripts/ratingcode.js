6fde66008f836694f7190fa025e001a36d796975
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-21 16:23:33 -0500

    d

--- PGCTour/Scripts/ratingcode.js
+++ PGCTour/Scripts/ratingcode.js
@@ -1,28 +1,29 @@
 var mySpreadsheetRatings = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1481979688';
 var ratingsTemplate = Handlebars.compile($('#ratings-pgcrank-template').html());
+var owgrTemplate = Handlebars.compile($('#ratings-owgr-template').html());
 
 $('#ratings-pgcrank').sheetrock({
   url: mySpreadsheetRatings,
-  query: "select A,B,C,D,E,F,G,H,I where B!='' order by A asc",
+  query: "select A,B,C where B!='' order by A asc",
   rowTemplate: ratingsTemplate
 });
 
 $('#ratings-owgr').sheetrock({
   url: mySpreadsheetRatings,
-  query: "select A,B,C,D,E,F,G,H,I where B!='' and E>0 order by D asc",
-  rowTemplate: ratingsTemplate
+  query: "select B,C,D where B!='' and D>0 order by D asc",
+  rowTemplate: owgrTemplate
 });
 
 $('#ratings-fedex').sheetrock({
   url: mySpreadsheetRatings,
-  query: "select A,B,C,D,E,F,G,H,I where B!='' and F>0 order by F asc",
-  rowTemplate: ratingsTemplate
+  query: "select B,C,F where B!='' and F>0 order by F asc",
+  rowTemplate: owgrTemplate
 });
 
 $('#ratings-strokesgain').sheetrock({
   url: mySpreadsheetRatings,
-  query: "select A,B,C,D,E,F,G,H,I where B!='' and H>0 order by H asc",
-  rowTemplate: ratingsTemplate
+  query: "select B,C,H where B!='' and H>0 order by H asc",
+  rowTemplate: owgrTemplate
 });
 
 function openCity(positionId) {
