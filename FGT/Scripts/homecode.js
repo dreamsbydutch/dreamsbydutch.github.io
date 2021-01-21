c8f0735b1448129dd89b0a1aa231672a690500d0
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-21 00:09:56 -0500

    h

--- FGT/Scripts/homecode.js
+++ FGT/Scripts/homecode.js
@@ -1,9 +1,9 @@
 var mySpreadsheetOverall = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=0';
-// var mySpreadsheetSchedule = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1192860014';
-// var mySpreadsheetWorldRank = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=119653565';
+var mySpreadsheetSchedule = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1192860014';
+var mySpreadsheetWorldRank = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=119653565';
 var overallStandingsTemplate = Handlebars.compile($('#cup-standings-template').html());
-// var scheduleTemplate = Handlebars.compile($('#schedule-template').html());
-// var worldrankTemplate = Handlebars.compile($('#worldrank-template').html());
+var scheduleTemplate = Handlebars.compile($('#schedule-template').html());
+var worldrankTemplate = Handlebars.compile($('#worldrank-template').html());
 
 $('#standings-slots').sheetrock({
   url: mySpreadsheetOverall,
@@ -11,17 +11,17 @@ $('#standings-slots').sheetrock({
   rowTemplate: overallStandingsTemplate
 });
 
-// $('#schedule-listing').sheetrock({
-//       url: mySpreadsheetSchedule,
-//       query: "select A,B,C,I,J,L where A <= 10 order by A",
-//       rowTemplate: scheduleTemplate
-// });
+$('#schedule-listing').sheetrock({
+      url: mySpreadsheetSchedule,
+      query: "select A,B,C,I,J,L where A <= 10 order by A",
+      rowTemplate: scheduleTemplate
+});
 
-// $('#worldrank-list').sheetrock({
-//   url: mySpreadsheetWorldRank,
-//   query: "select A,B,C,D",
-//   rowTemplate: worldrankTemplate
-// });
+$('#worldrank-list').sheetrock({
+  url: mySpreadsheetWorldRank,
+  query: "select A,B,C,D",
+  rowTemplate: worldrankTemplate
+});
 
 Handlebars.registerHelper('rankColorChange', function rankColorChange(change) {
       var i = parseInt(change);
