66a1623ca383eeed693047c9e4e8567b3da7ad3d
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-01-21 15:32:40 -0500

    s

--- PGCTour/Scripts/homecode.js
+++ PGCTour/Scripts/homecode.js
@@ -1,6 +1,6 @@
 var mySpreadsheetOverall = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=0';
 var mySpreadsheetSchedule = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1192860014';
-var mySpreadsheetWorldRank = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=119653565';
+var mySpreadsheetWorldRank = 'https://docs.google.com/spreadsheets/d/1LyloFyLI-YsPZnAWbmeh6l4KbhMUb1bqHm9Y47-fZOw/edit#gid=1481979688';
 var overallStandingsTemplate = Handlebars.compile($('#cup-standings-template').html());
 var scheduleTemplate = Handlebars.compile($('#schedule-template').html());
 var worldrankTemplate = Handlebars.compile($('#worldrank-template').html());
@@ -19,7 +19,7 @@ $('#schedule-listing').sheetrock({
 
 $('#worldrank-list').sheetrock({
   url: mySpreadsheetWorldRank,
-  query: "select A,B,C,D",
+  query: "select A,B,C where A <=10 order by A asc",
   rowTemplate: worldrankTemplate
 });
 
