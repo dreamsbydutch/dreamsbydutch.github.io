3f0eb632c8e2e732bf136bac12f2875af11f41a4
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2021-03-16 12:46:54 -0400

    s

--- PGCTour/Scripts/standingscode.js
+++ PGCTour/Scripts/standingscode.js
@@ -3,27 +3,27 @@ var standingsTemplate = Handlebars.compile($('#standings-template').html());
 
 $('#standings-list').sheetrock({
   url: mySpreadsheetStandings,
-  query: "select A,B,C,D,E,F,G,H,I,J,K order by A asc",
+  query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W order by A asc",
   rowTemplate: standingsTemplate
 });
 
 Handlebars.registerHelper('rankColorChange', function rankColorChange(change) {
-    var i = parseInt(change);
-    if (i > 0) {
-          return '#006700';
-    } else if (i < 0) {
-          return '#9a0000';
-    } else {
-          return '#222';
-    }
+  var i = parseInt(change);
+  if (i > 0) {
+    return '#006700';
+  } else if (i < 0) {
+    return '#9a0000';
+  } else {
+    return '#222';
+  }
 });
 
 function showTeam(rank) {
-  var x = document.getElementById("slot-"+rank+"-extras");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+  var x = document.getElementById("slot-" + rank + "-extras");
+  if (x.style.display == "none") {
+    x.style.display = "block";
   } else {
-    x.className = x.className.replace(" w3-show", "");
+    x.style.display = "none";
   }
 }
 
