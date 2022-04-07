8c7512d630732d71c2e7c04343dcbe1c96ad470f
Curtis Hough <chough14@gmail.com>
2022-04-07 01:07:31 -0400

    .

--- Masters/Scripts/code.js
+++ Masters/Scripts/code.js
@@ -1,9 +1,34 @@
-// Define spreadsheet URL.
 
-var leaderboardTemplate = Handlebars.compile($('#leaderboard-template').html());
+async function main() {
+      const x = await fetchData()
+      console.log(x)
+      var temp = document.getElementsByTagName("template")[0];
+      x.map(obj => {
+            var clone = temp.content.cloneNode(true);
+            var div = clone.querySelectorAll("div");
+            div[0].textContent = obj.Rank;
+            div[1].textContent = obj.Team;
+            div[2].textContent = obj.Earnings;
+            div[3].textContent = obj.Golfer1;
+            div[4].textContent = obj.Golfer2;
+            div[5].textContent = obj.Golfer3;
+            div[6].textContent = obj.Golfer4;
+            div[7].textContent = obj.Golfer5;
+            div[8].textContent = obj.Golfer6;
+            div[9].textContent = obj.Golfer7;
+            div[10].textContent = obj.Golfer8;
+            div[11].textContent = obj.Golfer9;
+            div[12].textContent = obj.Golfer10;
+            div[13].textContent = obj.Golfer11;
+            div[14].textContent = obj.Golfer12;
+            document.getElementById("leaderboard").appendChild(clone);
+      })
+}
 
-$('#leaderboard').sheetrock({
-      url: mySpreadsheet,
-      query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q order by Q asc",
-      rowTemplate: leaderboardTemplate
-});
+
+
+function fetchData() {
+      const x = fetch("https://opensheet.elk.sh/1NJZbWIvFYCTIt98BdxOZK1Z43XEOvqU6zqST_KJeJh0/1").then(res => res.json())
+      return x
+}
+main();
