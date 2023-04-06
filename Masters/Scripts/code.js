58d691b846da092c890a0779907bdc9ca69c8ed8
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2023-04-05 23:15:56 -0400

    Update code.js

--- Masters/Scripts/code.js
+++ Masters/Scripts/code.js
@@ -6,21 +6,21 @@ async function main() {
       x.map(obj => {
             var clone = temp.content.cloneNode(true);
             var div = clone.querySelectorAll("div");
-            div[14].textContent = obj.rank;
-            div[1].textContent = obj.teamName;
-            div[15].textContent = obj.earnings;
-            div[2].textContent = obj.golferOne;
-            div[3].textContent = obj.golferTwo;
-            div[4].textContent = obj.golferThree;
-            div[5].textContent = obj.golferFour;
-            div[6].textContent = obj.golferFive;
-            div[7].textContent = obj.golferSix;
-            div[8].textContent = obj.golferSeven;
-            div[9].textContent = obj.golferEight;
-            div[10].textContent = obj.golferNine;
-            div[11].textContent = obj.golferTen;
-            div[12].textContent = obj.golferEleven;
-            div[13].textContent = obj.golferTwelve;
+            div[1].textContent = obj.rank;
+            div[2].textContent = obj.teamName;
+            div[3].textContent = obj.earnings;
+            div[4].textContent = obj.golferOne;
+            div[5].textContent = obj.golferTwo;
+            div[6].textContent = obj.golferThree;
+            div[7].textContent = obj.golferFour;
+            div[8].textContent = obj.golferFive;
+            div[9].textContent = obj.golferSix;
+            div[10].textContent = obj.golferSeven;
+            div[11].textContent = obj.golferEight;
+            div[12].textContent = obj.golferNine;
+            div[13].textContent = obj.golferTen;
+            div[14].textContent = obj.golferEleven;
+            div[15].textContent = obj.golferTwelve;
             document.getElementById("leaderboard").appendChild(clone);
       })
 }
